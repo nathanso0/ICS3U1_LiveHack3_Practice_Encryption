bc13dbe29318a9c64f30c8c329c87479b2717d4b
nathanso0 <nathan.so1165@gmail.com>
2024-04-29 14:12:32 -0400

    itsastart

--- Encryption.java
+++ Encryption.java
@@ -4,9 +4,23 @@ class Encryption extends ConsoleProgram {
   * A program the encrypts a message inputted by the user
   * @author: 
   */
-  
+  String strmessagea;
+  int intshifta;
   public void run() {
-    
-  }
+  
+    System.out.println("**** Encryption Program *****");
+    strmessagea = readLine("Enter the message to encrypt: ");
+    intshifta = readInt("Enter the shift amount: ");
+    System.out.println((encryptedMessage(strmessagea, intshifta)));
+    }
 
+  public String encryptedMessage(String strMessage, int intShift) {
+    String strMessageCoded = "";
+
+    for (int intCounter = 0; intCounter > strMessage.length(); intCounter ++) {
+      strMessage.charAt(intCounter);
+    }
+    return strMessageCoded;
 }
+
+  }
